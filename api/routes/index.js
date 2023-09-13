f26c53a3054ba8e5bf478236c6e3bb188013cf3e
YaProjectorlaos <138667664+YaProjectorlaos@users.noreply.github.com>
2023-09-13 08:35:16 +0700

    Delete api directory

--- api/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", function (req, res) {
-  res.status(200).json({ api: "Welcome to API" });
-});
-
-module.exports = router;
